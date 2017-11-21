887180bca2e83059b8f1f1f947bc216cd6032f88
tom <tomallpress@hotmail.com>
2017-11-21 16:53:12 +0000

    bike docking test written

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -3,4 +3,8 @@ class DockingStation
   def release_bike
     Bike.new
   end
+
+  def dock(bike)
+
+  end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,4 +4,5 @@ describe DockingStation do
   it "should release a bike when method is called" do
     expect((DockingStation.new.release_bike).is_a? Bike).to eq true
   end
+  it {is_expected.to respond_to(:dock).with(1).argument}
 end
