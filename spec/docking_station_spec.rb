89d66d3f11bfbe9b4b0fa4f2747fd59572c2217c
Rhys Kyte <rhyskyte@Rhyss-MacBook-Pro.local>
2017-11-21 16:12:57 +0000

    release bike releases bike

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,4 +1,7 @@
 require "docking_station.rb"
 describe DockingStation do
   it { is_expected.to respond_to :release_bike }
+  it "should release a bike when method is called" do
+    expect((DockingStation.new.release_bike).is_a? Bike).to eq true
+  end
 end
